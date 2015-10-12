184ad095c6864510f9da0b61f8ff0fb3eb0c59cb
karieiriks <kari14@ru.is>
2015-10-12 14:44:08 +0000

    Test for empty string

---
+++ TestStringCalculator.java
@@ -0,0 +1,16 @@
+package is.ru.stringcalculator
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class StringCalculatorTest{
+
+	public static void main(String args[]){
+		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.StringCalculatorTest");
+	}
+
+		@Test
+		public void testEmptyString(){
+			assertEquals(0, StringCalculator.add(""));
+		}
+}
