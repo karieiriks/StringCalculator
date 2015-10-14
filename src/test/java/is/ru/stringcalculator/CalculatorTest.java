cc51c2438c208eec95b06033afba64d95e339122
karieiriks <kari14@ru.is>
2015-10-14 08:15:32 +0000

    changed from inlux to windows in compile

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	public static void main(String args[]) {
+
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
