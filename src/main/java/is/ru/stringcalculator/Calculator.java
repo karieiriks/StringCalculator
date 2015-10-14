58211d2f84f5a369aba0e913ed68c17cd6d53bc5
karieiriks <kari14@ru.is>
2015-10-14 10:55:34 +0000

    Code and refactoring for sum of string with 2 numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,9 +6,23 @@ public class Calculator {
 			if(text.equals("1")) {
 				return 1;
 			}
+			else if(text.contains(",")){
+				String[] parts = splitString(text);
+				return sum(toInt(parts[0]) , toInt(parts[1]));
+			}
 			return 0;
 	}
 
+	public static String[] splitString(String wholeString){
+		return wholeString.split(",");
+	}
 
+	public static int toInt(String str){
+		return Integer.parseInt(str);
+	}
+
+	public static int sum(Integer number1, Integer number2){
+		return number1 + number2;
+	}
 
 }
