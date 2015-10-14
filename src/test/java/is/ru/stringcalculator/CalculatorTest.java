fe4376e7d78856ac24af467e253885e82f1a495e
karieiriks <kari14@ru.is>
2015-10-14 12:43:27 +0000

    newline split test

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -35,4 +35,19 @@ public class CalculatorTest {
 		assertEquals(6, Calculator.add("1,2,3"));
 	}
 
+	@Test
+	public void testManyNumbers(){
+		assertEquals(55, Calculator.add("1,2,3,4,5,6,7,8,9,10"));
+	}
+
+	@Test
+	public void testNewLineChar(){
+		assertEquals(3, Calculator.add("1\n2"));
+	}
+
+	@Test 
+	public void testNewLineAndCommaChar(){
+		assertEquals(6, Calculator.add("1,2\n3"));
+	}
+
 }
